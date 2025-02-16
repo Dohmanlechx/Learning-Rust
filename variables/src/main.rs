4da4632e29b19374d698f5233987f1982063d9ab
David Öhman <dohman_92@hotmail.com>
2025-02-16 17:17:11 +0100

    ch03-03-how-functions-work.html

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -9,4 +9,37 @@ fn main() {
     }
 
     println!("The value of x is: {x}");
+
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3;
+    let remainder = 43 % 5;
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("{y}");
+    let one = tup.2;
+    println!("{one}");
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let a = [3; 5]; // [3, 3, 3, 3, 3]
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    std::io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
