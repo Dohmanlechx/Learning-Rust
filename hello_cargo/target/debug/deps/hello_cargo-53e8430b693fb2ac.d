12d05e80ba2c0b972ade3071624caf8bd35e952c
David Öhman <35524595+Dohmanlechx@users.noreply.github.com>
2025-02-18 22:36:56 +0100

    Delete hello_cargo/target directory

--- hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac.d
+++
@@ -1,5 +0,0 @@
-/Users/dohman/rust/hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac: src/main.rs
-
-/Users/dohman/rust/hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac.d: src/main.rs
-
-src/main.rs:
