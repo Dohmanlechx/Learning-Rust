5665a85b0246092b269cac6d0d1a80914bd2e904
David Öhman <dohman_92@hotmail.com>
2025-02-23 13:07:28 +0100

    ch04-03-fixing-ownership-errors.html

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-another_function(3);
+    another_function(3);
+
+    test_loop();
+    labeled_loops();
 }
 
 fn another_function(x: i32) {
@@ -9,4 +12,41 @@ fn another_function(x: i32) {
 
 fn five() -> i32 {
     5
+}
+
+fn test_loop() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+fn labeled_loops() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+
+    println!("End count = {count}");
 }
